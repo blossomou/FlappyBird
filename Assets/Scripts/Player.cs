0edd7e08f72f309e5fdf81db46dd1b848ddae82d
blossomou <qingqingou@gmail.com>
2022-01-27 17:38:11 -0500

    Add Sprite Animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] Sprite[] sprites;
+    private int spriteIndex;
     private Vector3 direction;
     [SerializeField]private float gravity = -9.8f;
     [SerializeField]private float strength = 5f;
 
+    private void Awake(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start(){
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
     private void Update(){
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
             direction = Vector3.up * strength;
@@ -21,5 +31,14 @@ public class Player : MonoBehaviour
 
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
+    }
+
+    private void AnimateSprite(){
+        spriteIndex++;
+        if(spriteIndex >= sprites.Length){
+            spriteIndex = 0;
+        }
+
+        spriteRenderer.sprite = sprites[spriteIndex];
     }
 }
