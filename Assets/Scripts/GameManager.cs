66325097c15a77b296fc427a6e8f20b222524e58
blossomou <qingqingou@gmail.com>
2022-01-27 22:30:54 -0500

    Add menu interactions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Player player;
+    [SerializeField] private Text scoreText;
+    [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject gameOver;
+
     private int score;
+
+    private void Awake() {
+        Application.targetFrameRate = 60;
+        Pause();
+    }
+
+    public void Play(){
+        score = 0;
+        scoreText.text = score.ToString();
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1f; 
+        player.enabled = true;
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+        
+        for(int i =0; i < pipes.Length; i++){
+            Destroy(pipes[i].gameObject);
+        }
+    }
+
+    public void Pause(){
+        Time.timeScale = 0f; //freezing the game
+        player.enabled = false;
+    }
+
     public void GameOver(){
-        Debug.Log("Game Over");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+        Pause();
     }
     public void IncreaseScore(){
         score++;
+        scoreText.text = score.ToString();
     }
 
 
