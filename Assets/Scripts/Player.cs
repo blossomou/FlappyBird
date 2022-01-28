5c558036a6dd198030f62202a9b94c58b93e2bbf
blossomou <qingqingou@gmail.com>
2022-01-27 22:38:18 -0500

    reset the position when player died

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,6 +16,14 @@ public class Player : MonoBehaviour
     private void Start(){
         InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
     }
+
+    //Reset the position when hit play
+    private void OnEnable(){
+        Vector3 position = transform.position;
+        position.y = 0f;
+        transform.position = position;
+        direction = Vector3.zero;
+    }
     private void Update(){
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
             direction = Vector3.up * strength;
