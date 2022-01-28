8a763bd5650bbc1b1968b2dd27a6eb9f4bc7d71b
blossomou <qingqingou@gmail.com>
2022-01-27 22:01:57 -0500

    Add Game state & Scoring

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -41,4 +41,13 @@ public class Player : MonoBehaviour
 
         spriteRenderer.sprite = sprites[spriteIndex];
     }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "Obstacle"){
+            FindObjectOfType<GameManager>().GameOver();
+
+        }else if(other.gameObject.tag == "Scoring"){
+            FindObjectOfType<GameManager>().IncreaseScore();
+        }
+    }
 }
