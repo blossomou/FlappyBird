4f48fa570777002cd4187dc1bf795df200f62c5a
blossomou <qingqingou@gmail.com>
2022-01-27 22:50:46 -0500

    fixed Misspelled OnEnable and OnDisable func

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,20 +7,14 @@ public class Spawner : MonoBehaviour
     [SerializeField]private GameObject prefab;
     [SerializeField]private float spawnRate = 1f;
     [SerializeField]private float minHeight = -1f;
-
     [SerializeField]private float maxHeight = 1f;
 
-    //Doesn't work use onEnable, the pipes won't generate
-    // private void onEnable(){
-    //     Debug.Log("Enabled is calling");
-    //      InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
-    // }
-
-    private void Start(){
-        InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
+   
+    private void OnEnable(){
+         InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
     }
-    
-    private void onDisable(){
+
+    private void OnDisable(){
          CancelInvoke(nameof(Spawn));
     }
 
